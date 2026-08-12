17acd65a2e5ad10df4e3e6f3cea7b50e37abc35e
Erzhan004 <220431@astanait.edu.com>
2026-08-12 15:48:21 +0500

    g

--- public/webhook.php
+++ public/webhook.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 ini_set('display_errors', '0');
 error_reporting(E_ALL);
 
+// Hoster.kz may run PHP 8.2 while composer.lock was built with 8.3 platform check.
+putenv('COMPOSER_DISABLE_PLATFORM_CHECK=1');
+
 require dirname(__DIR__) . '/vendor/autoload.php';
 
 use App\Config;
